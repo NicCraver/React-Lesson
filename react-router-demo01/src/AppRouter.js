e99903f9c3ae72df596f3928ef4496090e35c6a0
Nic_ <36131101+NicCraver@users.noreply.github.com>
2020-01-10 11:46:12 +0800

    初始化项目 安装 react-router-dom

---
+++ react-router-demo01/src/AppRouter.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class AppRouter extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        222
+      </div>
+    )
+  }
+}
